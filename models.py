5e7cc30421298fa22eadd029a696e4219182a78a
lucscordeiro <lucas.cordeiro3030@gmail.com>
2024-11-04 20:18:51 -0300

    iniciando projeto

--- models.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, nome, cpf, email, celular):
-        self.nome = nome
-        self.cpf = cpf
-        self.email = email
-        self.celular = celular
-
-    def __str__(self):
-        return f"Nome: {self.nome}, CPF: {self.cpf}, Email: {self.email}, Celular: {self.celular}"
